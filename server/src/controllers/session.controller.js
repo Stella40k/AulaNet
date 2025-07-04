8eb37d896bb6899130cfc1db7ba008a1874a054a
Bernard <bernardstella0704@gmail.com>
2025-07-04 11:37:35 -0300

    feat: estructura de las rutas

---
+++ server/src/controllers/session.controller.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+
+
+import {
+    login,
+    logout,
+    session,
+    register,
+} from //aca va la importacion de los controladores
+
+export const routeSession = Router()
+
+routeSession.post("/login", login);
+routeSession.get("/session", session);
+routeSession.post("/logout", logout);
+routeSession.post("/register", register);
