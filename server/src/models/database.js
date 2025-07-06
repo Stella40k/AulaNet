a4d19fa5a989c423a51236c179eaf70b036183f7
Bernard <bernardstella0704@gmail.com>
2025-07-06 14:05:40 -0300

    fix: estructura de la conexion de la bd con sequielize

--- server/src/models/database.js
+++ server/src/models/database.js
@@ -2,10 +2,10 @@ import { Sequelize } from "sequelize";
 import "dotenv/config";
 
 export const sequelize = new Sequelize(
-    process.env.DB_HOST,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+
     process.env.DB_NAME,
+    process.env.DB_USER,    
+    process.env.DB_PASSWORD,
     {
         host: process.env.DB_HOST,
         dialect: process.env.DB_DIALECT
